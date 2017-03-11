f71bb197adb7809685c4e1c6f4f066872ef1bf97
yswu1022eng <yswu1022eng@gmail.com>
2017-03-11 17:02:52 +0800

    加入輸出功能

--- CS280-Homework-2/Program.cs
+++ CS280-Homework-2/Program.cs
@@ -59,6 +59,12 @@ namespace CS280_Homework_2
                 Console.WriteLine("錯誤訊息:\n" + e.ToString());
                 return;
             }
+            /// 性別
+            Console.Write("Gender:");
+            pokemons[0].Gender = Console.ReadLine();
+
+            ///輸出
+            Console.WriteLine("您剛剛輸入的Pokemon資訊：\n名字：{0}\n編號：{1}\n特性：{2}\n分類：{3}\n身高：{4}\n體重：{5}\n性別：{6}", pokemons[0].Name, pokemons[0].Numero, pokemons[0].Abilities, pokemons[0].Category, pokemons[0].Height, pokemons[0].Weight, pokemons[0].Gender);
             Console.WriteLine("謝謝您的輸入");
         }
     }
