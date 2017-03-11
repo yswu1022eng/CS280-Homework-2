c9e54fa8158b8edf3e01ac53e043f5edabe2fc9f
yswu1022eng <yswu1022eng@gmail.com>
2017-03-11 15:15:32 +0800

    Add project files.

---
+++ CS280-Homework-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
